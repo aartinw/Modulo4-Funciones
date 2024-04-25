69aa8966162981ed6160906807d0e1bbdab03af8
Aarti Nanwani Samtani <aarti@MacBook-Pro-de-Aarti.local>
2024-04-25 17:55:28 +0200

    cambios

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 let turno = 1;
 
 function actualizarDisplay(): void {
-  const turnoElement: HTMLElement | null = document.getElementById("turno");
-  if (turnoElement) {
+  const turnoElement = document.getElementById("turno");
+  if (turnoElement && turnoElement instanceof HTMLHeadingElement) {
     turnoElement.textContent = turno.toString().padStart(2, "0");
   }
 }
 
 function siguienteTurno(): void {
-  turno = turno + 1;
+  turno++;
   actualizarDisplay();
 }
 
 function anteriorTurno(): void {
   if (turno > 0) {
-    turno = turno - 1;
+    turno--;
     actualizarDisplay();
   }
 }
@@ -25,17 +25,32 @@ function resetTurno(): void {
 }
 
 function cambiarTurno(): void {
-  const turnoOperario: HTMLInputElement | null = document.getElementById(
-    "texto"
-  ) as HTMLInputElement;
-  const nuevoTurno = parseInt(turnoOperario?.value);
-  if (typeof nuevoTurno === "number" && nuevoTurno > 0) {
-    turno = nuevoTurno;
-    actualizarDisplay();
+  const turnoOperario = document.getElementById("texto");
+  if (turnoOperario && turnoOperario instanceof HTMLInputElement) {
+    const nuevoTurno = parseInt(turnoOperario.value);
+    if (typeof nuevoTurno === "number" && nuevoTurno > 0) {
+      turno = nuevoTurno;
+      actualizarDisplay();
+    }
   }
 }
 
-document.getElementById("siguiente")?.addEventListener("click", siguienteTurno);
-document.getElementById("anterior")?.addEventListener("click", anteriorTurno);
-document.getElementById("reset")?.addEventListener("click", resetTurno);
-document.getElementById("cambiar")?.addEventListener("click", cambiarTurno);
+const botonSiguiente = document.getElementById("siguiente");
+if (botonSiguiente && botonSiguiente instanceof HTMLButtonElement) {
+  botonSiguiente.addEventListener("click", siguienteTurno);
+}
+
+const botonAnterior = document.getElementById("anterior");
+if (botonAnterior && botonAnterior instanceof HTMLButtonElement) {
+  botonAnterior.addEventListener("click", anteriorTurno);
+}
+
+const botonReset = document.getElementById("reset");
+if (botonReset && botonReset instanceof HTMLButtonElement) {
+  botonReset.addEventListener("click", resetTurno);
+}
+
+const botonCambiar = document.getElementById("cambiar");
+if (botonCambiar && botonCambiar instanceof HTMLButtonElement) {
+  botonCambiar.addEventListener("click", cambiarTurno);
+}
